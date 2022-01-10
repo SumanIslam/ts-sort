fec02bae2f68128407b7efbb5c9631c61b1fecdb
Sumna Islam <sumanislam1416@gmail.com>
2022-01-10 09:49:01 +0600

    solving all issues with abstract class

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,14 @@ import { LinkedList } from "./LinkedList";
 
 
 // number
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 // character
-// const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
-
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 // linkedList
 const linkedList = new LinkedList();
@@ -24,6 +21,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
